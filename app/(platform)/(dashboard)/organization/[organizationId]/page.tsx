8612aaafb678b3193f958904e20f17da34238ab0
gd <gawdwnn@gmail.com>
2024-08-23 12:28:05 -0400

    env setup, auth with clerk, layout

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -0,0 +1,8 @@
+import { auth } from "@clerk/nextjs";
+import React from "react";
+
+const OrganizationIdPage = () => {
+  return <div>Organization Page</div>;
+};
+
+export default OrganizationIdPage;
