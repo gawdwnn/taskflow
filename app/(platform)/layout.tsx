d55c0f89ff32a2b0055afc4d86623b0d878f4abf
gd <gawdwnn@gmail.com>
2024-08-27 00:55:39 -0400

    List Feature

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -2,7 +2,12 @@ import { Toaster } from "sonner";
 import { ClerkProvider } from "@clerk/nextjs";
 
 const PlatformLayout = ({ children }: { children: React.ReactNode }) => {
-  return <ClerkProvider>{children}</ClerkProvider>;
+  return (
+    <ClerkProvider>
+      <Toaster />
+      {children}
+    </ClerkProvider>
+  );
 };
 
 export default PlatformLayout;
